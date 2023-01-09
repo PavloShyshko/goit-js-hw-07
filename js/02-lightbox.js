6e9516e8755aa69d34ab72779b7223d56b514fe3
PavloShyshko <111237173+PavloShyshko@users.noreply.github.com>
2023-01-09 23:11:59 +0100

    finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const createMarkup = galleryItems
+  .map(
+      (item) =>
+`<a class="gallery__item" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+</a>`
+).join("");
+  
+galleryContainer.insertAdjacentHTML("beforeend", createMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
+
+  
+
+
+
